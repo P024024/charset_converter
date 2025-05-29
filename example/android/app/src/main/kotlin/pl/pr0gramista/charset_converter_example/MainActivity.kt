5b3ac4cfb4fd20723770939bedbda2c79999ab86
Pawan Ghimire <pghimire024@rku.ac.in>
2025-05-29 11:51:02 +0545

    small

--- example/android/app/src/main/kotlin/pl/pr0gramista/charset_converter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/pl/pr0gramista/charset_converter_example/MainActivity.kt
@@ -2,4 +2,4 @@ package pl.pr0gramista.charset_converter_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
